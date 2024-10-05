e3937110998221d405f6771011a107b6ebb09b9b
minhtung <mtung2802@gmail.com>
2024-10-05 22:29:37 +0700

    Tung

---
+++ grade.java
@@ -0,0 +1,47 @@
+public class Grade {
+    private String student_ID;
+    private String id;
+    private String className;
+    private float marks;
+    private String remarks;
+
+    public String getStudent_ID() {
+        return student_ID;
+    }
+
+    public void setStudent_ID(String student_ID) {
+        this.student_ID = student_ID;
+    }
+
+    public String getID() {
+        return id;
+    }
+
+    public void setID(String id) {
+        this.id = id; // 
+    }
+
+    public String getClassName() {
+        return className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
+
+    public float getMarks() {
+        return marks;
+    }
+
+    public void setMarks(float marks) {
+        this.marks = marks;
+    }
+
+    public String getRemarks() {
+        return remarks;
+    }
+
+    public void setRemarks(String remarks) {
+        this.remarks = remarks;
+    }
+}
