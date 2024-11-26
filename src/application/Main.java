aa93297c2bcab48acfed76808725fe9afe3c7b85
“Cao <tacaoson230804@gmail.com>
2024-11-26 09:12:20 +0700

    done

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,12 +1,15 @@
 package application;
+
+import java.sql.SQLException;
+
 import Frames.LoginForm;
 
 public class Main {
-    public static void main(String[] args) {
-        // Launch the Login Form
+    public static void main(String[] args) throws SQLException {
         javax.swing.SwingUtilities.invokeLater(() -> {
             new LoginForm();
             // new MainForm();
         });
+        Database dtb = new Database();
     }
 }
